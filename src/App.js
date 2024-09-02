913ffe8436a48faba1ec080a798b169b5a287d5e
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-09-02 16:55:15 -0300

    termine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [term, setTerm] = useState("")
-
+  const [consejo, setConsejo] = useState("")
+  const [consejos, setConsejos] = useState([])
   const handleTermChange = (event) => setTerm(event.target.value);
-
+  const obtenerConsejo = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        else {
+          throw new ("Error")
+        }
+      })
+      .then(data => setConsejo(data["slip"].advice))
+      .catch(error => alert("Error:" + error))
+  }
+  const obtenerConsejos = () => {
+    fetch("https://api.adviceslip.com/advice/search/" + term)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        else {
+          throw new ("Error")
+        }
+      })
+      .then(data => {
+        if (data["slips"]) {
+          setConsejos(data["slips"])
+        }
+        else {
+          alert("No se encontraron resultados")
+        }
+      })
+      .catch(error => alert("Error:" + error))
+  }
+  useEffect(() => {
+    obtenerConsejo()
+  }, consejo === "")
   return (
     <main>
 
@@ -15,16 +51,16 @@ function App() {
 
       <div>
         <h2>Obtener un consejo aleatorio</h2>
-        <button>Obtener</button>
-        <p className="result-box"></p>
+        <button onClick={obtenerConsejo}>Obtener</button>
+        <p className="result-box">{consejo}</p>
       </div>
 
       <div>
         <h2>Buscar un consejo</h2>
         <input type="text" onChange={handleTermChange} />
-        <button>Enviar</button>
+        <button onClick={obtenerConsejos}>Enviar</button>
         <h3>Resultados de búsqueda:</h3>
-        <p className="result-box"></p>
+        <p className="result-box">{consejos.map((consejo) => <p>{consejo["advice"]}</p>)}</p>
       </div>
 
     </main>
